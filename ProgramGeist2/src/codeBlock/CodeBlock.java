afeb5dfb71169797aefdd0221b6560b0d8a72d17
millegab001 <captaintechnicality@gmail.com>
2015-01-22 07:18:49 -0800

    Worked on new hitbox system.

--- ProgramGeist2/src/codeBlock/CodeBlock.java
+++ ProgramGeist2/src/codeBlock/CodeBlock.java
@@ -1,6 +1,7 @@
 package codeBlock;
 
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.*;
 
 import world.*;
 import entity.*;
@@ -22,12 +23,13 @@ public class CodeBlock extends Entity{
 	protected CodeBlock downBlock = null;
 	protected CodeBlock upBlock   = null;
 	
+	
 	// Constructors
 	public CodeBlock(EntityWorld world) throws SlickException {
 		this(0, 0, world);
 	}
 	public CodeBlock(float x, float y, EntityWorld world) {
-		super(x, y, world);
+		super(x, y, hitbox, radius, false, world);
 	}
 	public CodeBlock(CodeBlock downBlock, EntityWorld world) throws SlickException {
 		this(downBlock.getX(), downBlock.getY()-20, world);
