43fd08c7958a469ec85975c99a8627d8a6c737b1
millegab001 <captaintechnicality@gmail.com>
2015-04-02 06:32:06 -0700

    Tiles are back.

---
+++ ProgramGeist2/src/tile/TriangleTile.java
@@ -0,0 +1,37 @@
+package tile;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Vector2f;
+
+import entity.Entity;
+import world.EntityWorld;
+
+public class TriangleTile extends Entity {
+	
+	public TriangleTile(int x, int y, EntityWorld world) throws SlickException {
+		super(x, y, makeTriangle(x + 50, y + 50, -50, 50, 50, 50, 50, -50), world);
+		
+		midpoint = new Vector2f(position.x + 75, position.y + 75);
+		//Slope faces up-left
+		image = new Image("res/TriangleTile.png");
+		
+		entityType = EntityType.Tile;
+	}
+
+	@Override
+	public boolean update(int deltaMS) {
+		// TODO Tile update
+		return true;
+	}
+
+	@Override
+	public boolean update(int deltaMS, Input input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+}
